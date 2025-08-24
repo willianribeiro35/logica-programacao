24a0bb0ac1e42cbb0a226655bdfc03b6fb8b2ab9
Willian Ribeiro <willianribeirows29@gmail.com>
2025-08-24 20:25:09 -0300

    :sparkles: feat: primeiro commit

---
+++ aula2/atividade.js
@@ -0,0 +1,5 @@
+let nomes = ["willian","sophia","gabriel"]
+let idade = [35,13,16]
+let profissao = ["pedreiro","jovem aprediz","vendedora"]
+
+console.log(nomes[0]+idade[1]+profissao[2])
---
+++ aula2/aula2.js
@@ -0,0 +1,9 @@
+let listaDeCopra = ["arroz","feijão","açucar","macarão","manteiga","oleo","leite"];
+
+let pessoa = {
+    nome: "Willian Ribeiro Da Silva Mendes",
+    idade:35,
+    profissao: "Pedreiro"
+
+}
+console.log(pessoa)
